d7580cde57a63783e048b63a84317082cff26230
DavidDHDH <david.heimfert@gmail.com>
2025-04-15 17:50:40 -0400

    fix tsc

--- app/projects/[projectName]/page.tsx
+++ app/projects/[projectName]/page.tsx
@@ -1,15 +1,14 @@
-import React from "react"
-import { projects } from "@/datas/projects"
+import { projects } from "@/datas/projects";
 
 const ProjectPage = ({ params }: { params: { projectName: string } }) => {
-	const project = projects.find(
-		(project) => project.internalUrl === params.projectName
-	)
-	return (
-		<main className="h-screen flex justify-center items-center">
-			ProjectPage {project?.title}
-		</main>
-	)
-}
+  const project = projects.find(
+    (project) => project.slug === params.projectName
+  );
+  return (
+    <main className="h-screen flex justify-center items-center">
+      ProjectPage {project?.title}
+    </main>
+  );
+};
 
-export default ProjectPage
+export default ProjectPage;
