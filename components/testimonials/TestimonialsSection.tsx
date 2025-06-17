53ed5f10e9b9162463422fce7c25ff358ea4bc29
DavidDHDH <david.heimfert@gmail.com>
2025-06-17 08:52:40 -0400

    refactor: review schema org

--- components/testimonials/TestimonialsSection.tsx
+++ components/testimonials/TestimonialsSection.tsx
@@ -15,18 +15,34 @@ export default function TestimonialsSection() {
             La confiance est mutuelle.
           </p>
         </div>
-        <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none ">
-          <div className="sm:-mx-4 sm:columns-2 sm:text-[0]  lg:columns-3">
-            {testimonials.map((testimonial, index) => (
-              <div
-                key={index}
-                className={`pt-8 sm:inline-block sm:w-full sm:px-4 ${
-                  index === 3 && 'col-start-2'
-                }`}
-              >
-                <TestimonialCard testimonial={testimonial} />
-              </div>
-            ))}
+        <div itemScope itemType="https://schema.org/Product">
+          <meta
+            itemProp="name"
+            content="Création de sites internet et solutions IA sur‑mesure"
+          />
+          <div
+            itemProp="aggregateRating"
+            itemScope
+            itemType="https://schema.org/AggregateRating"
+          >
+            <meta itemProp="ratingValue" content="5" />
+            <meta itemProp="ratingCount" content="6" />
+            <meta itemProp="bestRating" content="5" />
+            <meta itemProp="worstRating" content="1" />
+          </div>
+          <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none ">
+            <div className="sm:-mx-4 sm:columns-2 sm:text-[0]  lg:columns-3">
+              {testimonials.map((testimonial, index) => (
+                <div
+                  key={index}
+                  className={`pt-8 sm:inline-block sm:w-full sm:px-4 ${
+                    index === 3 && 'col-start-2'
+                  }`}
+                >
+                  <TestimonialCard testimonial={testimonial} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
