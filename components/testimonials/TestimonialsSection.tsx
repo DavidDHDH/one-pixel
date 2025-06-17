3db21c70af6cb3c515e1c275bd29385b47404c5f
DavidDHDH <david.heimfert@gmail.com>
2025-06-17 12:15:36 -0400

    feat: add google logo to testimonials

--- components/testimonials/TestimonialsSection.tsx
+++ components/testimonials/TestimonialsSection.tsx
@@ -1,5 +1,6 @@
 import { testimonials } from '@/datas/testimonials'
 import { CONTACT_EMAIL } from '@/lib/const'
+import Image from 'next/image'
 import TestimonialCard from '../hero-section/TestimonialCard'
 import { Icon } from '../shared/Icon'
 
@@ -7,13 +8,27 @@ export default function TestimonialsSection() {
   return (
     <section id="testimonials" className="bg-white">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="sm:text-lg font-semibold leading-7 text-primaryText">
-            Avis & témoignages clients
-          </h2>
-          <p className="font-rubik mt-2 text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
-            La confiance est mutuelle.
-          </p>
+        <div className="mx-auto max-w-2xl text-center flex flex-col items-center justify-center gap-2">
+          <div className="flex flex-col items-center justify-center">
+            <h2 className="sm:text-lg font-semibold leading-7 text-primaryText">
+              Avis & témoignages clients
+            </h2>
+            <p className="font-rubik mt-2 text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
+              La confiance est mutuelle.
+            </p>
+          </div>
+          <a
+            href="https://g.co/kgs/XJQCvZ6"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/images/Google-Review-Logo.png"
+              alt="Google Review Logo"
+              width={100}
+              height={100}
+            />
+          </a>
         </div>
         <div itemScope itemType="https://schema.org/Product">
           <meta
